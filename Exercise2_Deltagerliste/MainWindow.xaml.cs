1fd2499b62a58d00ba253136f3681bb16295125c
Jim <partyblues@gmail.com>
2020-09-29 14:59:03 +0200

    Sætter alt ind i master

--- Exercise2_Deltagerliste/MainWindow.xaml.cs
+++ Exercise2_Deltagerliste/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Exercise2_Deltagerliste
 {
@@ -24,5 +15,51 @@ namespace Exercise2_Deltagerliste
         {
             InitializeComponent();
         }
+
+        private void LoadFiler(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = null;
+            StreamReader s = null;
+            try
+            {
+                fs = new FileStream(@"C:\Users\Jim\source\repos\Gui_Øvelser\deltagerliste.csv", FileMode.Open, FileAccess.Read);
+                s = new StreamReader(fs, Encoding.Default);
+                string str;
+                string[] tokens;
+                char[] separators = { ',' };
+                List<string> lines = new List<string>();
+                string nextLine = "";
+                str = s.ReadLine();
+
+                while (!s.EndOfStream)
+                {
+                    str = s.ReadLine();
+                    str = str.Replace("\"", string.Empty);
+                    if (str != "")
+                    {
+                        tokens = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (tokens[1].Length > 20)
+                            tokens[1] = tokens[1].Substring(0, 20); // Limit first name to max 20 chars
+
+                        nextLine = $"{tokens[1],-20} {tokens[0],-20}  {tokens[2],-12}";
+                        lines.Add(nextLine);
+                    }
+
+                }
+
+                lbxDeltagere.ItemsSource = lines;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "An error occured during file IO");
+            }
+            finally
+            {
+                s.Close();
+                fs.Close();
+            }
+        }
+
     }
 }
