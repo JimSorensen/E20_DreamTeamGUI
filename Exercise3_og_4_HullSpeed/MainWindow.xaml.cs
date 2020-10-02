e8a84bfc05085e1ef68f6cb3f2d34cfae12069dc
Jim <partyblues@gmail.com>
2020-10-02 15:09:47 +0200

    Sætter ændringer op fra opgeve 4

--- Exercise3_og_4_HullSpeed/MainWindow.xaml.cs
+++ Exercise3_og_4_HullSpeed/MainWindow.xaml.cs
@@ -1,34 +1,16 @@
 ﻿using System.Windows;
-using System.Windows.Input;
 
 namespace HullSpeed
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
-
     public partial class MainWindow : Window
     {
-
-
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
         public void calculateButton_Click(object sender, RoutedEventArgs e)
         {
-
-
-
-            //string userInput = textBoxLength.ToString();
-            //Console.WriteLine(userInput);
-
-
-
             string userInputString = textBoxLength.Text;
 
             double userInputDouble = double.Parse(userInputString);
@@ -37,33 +19,11 @@ namespace HullSpeed
 
             tbxHullSpeed.Text = (knots).ToString();
 
-
-
-
-
-            MessageBox.Show("The name of this ship is " + tbxName.Text + "\n" + "Total knots: " + tbxHullSpeed.Text);
-
-
-
-
-            //int calculateLength =  1.34 * System.Math.Sqrt(Length);
-
-            //tbxHullSpeed.Text = length;
-            //MessageBox.Show(x);
+            MessageBox.Show("The name of this ship is " + tbxName.Text + "\n"
+                            + "Total knots: " + tbxHullSpeed.Text);
         }
-
-        //private void stackPanelMain_KeyDown(object sender, KeyEventArgs e)
-        //{
-        //    if (e.Key == Key.L)
-        //    {
-        //        //MessageBox.Show("KeyDown L");
-        //        ZoomIn();
-        //    }
-        //    else if (e.Key == Key.S)
-        //    {
-        //        //MessageBox.Show("KeyDown: S");
-        //        ZoomOut();
-        //    }
-        //}
     }
 }
+
+//HVordan sætter billeder ind
+/* https://www.youtube.com/watch?v=affbgRZoeGc */
